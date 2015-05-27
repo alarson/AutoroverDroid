aa0f33bf117f9974dce925c69f2637bb1adc48ff
ari <arielsebastianj@gmail.com>
2015-05-27 01:24:41 +0000

    managed to get UDP communication from pi to windows. sendingTestUDP.py is pi side

---
+++ sendingTestUDP.py
@@ -0,0 +1,16 @@
+#first attempt 5/26
+#Ari Larson
+#code for udp communication referenced from wiki.python.org
+
+import socket
+
+UDP_IP="10.0.1.17"
+UDP_PORT=5005
+MESSAGE="hello world"
+
+print "UDP target ip: ",UDP_IP
+print "UDP target port: ", UDP_PORT
+print "message: ",MESSAGE
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.sendto(MESSAGE,(UDP_IP, UDP_PORT))
