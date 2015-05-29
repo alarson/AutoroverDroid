497851b8154ee05584c2597f921ef344c6e5d7bc
Ari Larson <arielsebastianj@gmail.com>
2015-05-28 20:15:42 -0400

    added the recieving python code for simple udp connection

---
+++ receivingTestUDP.py
@@ -0,0 +1,16 @@
+#ari larson
+#5/26/2015
+#windows side listening simple UDP in python
+
+import socket
+ 
+UDP_IP = "10.0.1.17"
+UDP_PORT = 5005
+ 
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+ 
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", data
